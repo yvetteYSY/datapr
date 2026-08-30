d53fb2c0503664147f31a8700ebd9c15c198c10e
yvetteYSY <59806711+yvetteYSY@users.noreply.github.com>
2026-08-29 22:23:21 -0700

    test: demonstrate a dangerous orders change

--- examples/shop_analytics/models/marts/orders.sql
+++ examples/shop_analytics/models/marts/orders.sql
@@ -1,7 +1,7 @@
 select
     o.order_id,
-    o.customer_id,
+    cast(o.customer_id as varchar) as customer_id,
     o.order_total,
     o.created_at
 from {{ ref('stg_orders') }} as o
-where o.created_at >= current_date - interval '7 days'
+cross join {{ source('raw', 'customer_segments') }} as segments
